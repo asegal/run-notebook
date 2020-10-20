1dd6f5b7326a42685544743d5b4e3e45ac27cdfa
Amit Segal <asegal@glgroup.com>
2020-10-20 14:56:01 -0400

    classic template

--- src/main.ts
+++ src/main.ts
@@ -52,7 +52,7 @@ async function run() {
     // Execute notebooks
     await Promise.all(notebookFiles.map(async (notebookFile: string) => {
       const parsedNotebookFile = path.join(outputDir, path.basename(notebookFile));
-      await exec.exec(`jupyter nbconvert --execute --no-input --allow-chromium-download --to webpdf --output "${parsedNotebookFile}.pdf" "${notebookFile}"`);
+      await exec.exec(`jupyter nbconvert --execute --no-input --allow-chromium-download --to webpdf --template classic --output "${parsedNotebookFile}" "${notebookFile}"`);
     })).catch((error) => {
       core.setFailed(error.message);
     });
